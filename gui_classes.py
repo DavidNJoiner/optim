73ab8be08831743adf96cabf3066126f74b419f6
Dvid3r <Dvid3r@gmail.com>
2022-01-20 03:50:05 -0800

    testing poo gui

--- gui_classes.py
+++ gui_classes.py
@@ -1,22 +1,24 @@
 import tkinter as tk
+from tkinter import ttk
+from turtle import bgcolor
 import ezdxf
+import partitioning
 
-class MainApp(tk.Frame):
+class MainApp():
 
-    def __init__(self, root):
-
-        tk.Frame.__init__(self, root)
-        self.root = root
-        
-        toolbox = Toolbox(self.root)
-        #toolbox.pack(side="left", fill="y")
-
-        self.title = ('Autocad Helper')
-        self.geometry = ('400x400')
+    def __init__(self, master):
 
         self.dxf_dat = None
         self.doc = ezdxf.new(dxfversion='R2010')
         self.msp = self.doc.modelspace()
+
+        mainframe = ttk.Frame(master)
+
+        toolbox = Toolbox(mainframe)
+        toolbox2 = Toolbox(mainframe)
+
+        toolbox.grid(column = 0, row = 0)
+        toolbox2.grid(column = 1, row = 0)
 
     def exportDXF(self):
         
@@ -25,16 +27,33 @@ class MainApp(tk.Frame):
         self.doc.saveas('template.dxf')
 
    
-class Toolbox(tk.Toplevel):
+class Toolbox(tk.Frame):
 
     def __init__(self, parent):
+
+        super(Toolbox, self).__init__()
+
+        toolBoxframe = ttk.Frame(parent)
+        
+        #Create all the toolbox widgets
+        self.wallA_lbl = ttk.Label(toolBoxframe, text='Wall A : ')
+        self.wallB_lbl = ttk.Label(toolBoxframe, text='Wall B : ')
+        self.wallA_entry = ttk.Entry(toolBoxframe, text='')
+        self.wallB_entry = ttk.Entry(toolBoxframe, text='')
+        self.processBtn = ttk.Button(toolBoxframe, text='PROCESS', command=self.onReturn)
+
+        #Layout all the toolbox widgets
+        self.wallA_lbl.grid(column = 0, row = 0)
+        self.wallB_lbl.grid(column = 0, row = 1)
+        self.wallA_entry.grid(column = 1, row = 0)
+        self.wallB_entry.grid(column = 1, row = 1)
+        self.processBtn.grid(column = 0, row = 2)
 
         self.wallA = 0
         self.wallB = 0
 
-    def onReturn(storageVal, entry):  
-        storageVal = entry.get()
-        print(storageVal+' was stored')
+    def onReturn(self):  
+        print('something was stored')
 
     def clrEntries(self):
 
